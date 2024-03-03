bd3a7905becbc370d1d6675e2da1a35b8fafe3ff
Almer Badart <almer.badart@ordina.nl>
2024-03-03 21:25:58 +0100

    Added mobile navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,25 @@
-
 const mobileNavToggle = document.getElementById('toggleMainNav');
-mobileNavToggle.addEventListener('click', function() {
-    document.body.classList.toggle('nav-open');
+
+function openNavigation() {
+    mobileNavToggle.setAttribute('aria-expanded', 'true');
+    document.body.classList.add('nav-open');
+}
+
+function closeNavigation() {
+    mobileNavToggle.setAttribute('aria-expanded', 'false');
+    document.body.classList.remove('nav-open');
+}
+
+function toggleNavigation() {
+    const open = mobileNavToggle.getAttribute('aria-expanded');
+    open === 'false' ? openNavigation() : closeNavigation();
+}
+
+mobileNavToggle.addEventListener('click', toggleNavigation);
+
+window.addEventListener('keyup', (e) => {
+    if (e.key === 'Escape') {
+        mobileNavToggle.focus();
+        closeNavigation();
+    }
 });
