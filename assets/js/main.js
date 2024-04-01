152ecebacaa3222f937b294461ba290d8c215921
Almer Badart <almer.badart@ordina.nl>
2024-04-01 20:27:45 +0200

    Added newsletter form

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,17 @@
 
 const mobileNavToggle = document.getElementById('toggleMainNav');
 
-function openNavigation() {
+const openNavigation = () => {
     mobileNavToggle.setAttribute('aria-expanded', 'true');
     document.body.classList.add('nav-open');
 }
 
-function closeNavigation() {
+const closeNavigation = () => {
     mobileNavToggle.setAttribute('aria-expanded', 'false');
     document.body.classList.remove('nav-open');
 }
 
-function toggleNavigation() {
+const toggleNavigation = () => {
     const open = mobileNavToggle.getAttribute('aria-expanded');
     open === 'false' ? openNavigation() : closeNavigation();
 }
@@ -27,30 +27,37 @@ window.addEventListener('keyup', (e) => {
 });
 
 // Mailchimp post and validation
-const newsLetterForm = document.getElementById('');
+const newsLetterForm = document.forms.nlForm;
+const mc_c = "jQuery19003303681766368718_";
 
-// Example POST method implementation:
-async function postFormData() {
+newsLetterForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    sendFormData(event.target);
+});
 
-    const url = "https://doedelskindermode.us7.list-manage.com/subscribe/post-json?u=d9195e5285b008d78540931c4&id=13304b5a94&f_id=000bd9e4f0&c=jQuery190021296043349292648_1710357131636&EMAIL=almer1%40almerbadart.nl&FNAME=Almer&LNAME=Badart&b_d9195e5285b008d78540931c4_13304b5a94=&subscribe=Subscribe&_=1710357131637";
 
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        // body: JSON.stringify(postData), // body data type must match "Content-Type" header
-    });
-    return response // parses JSON response into native JavaScript objects
+const sendFormData = async (form) => {
+    
+    const formData = new FormData(form);
+    const params = new URLSearchParams(formData);
+    params.append("c",mc_c+Date.now());
+    params.append("_",Date.now());
+
+    const requestUrl = form.action+"&"+params.toString();
+
+    // const response = await fetch(requestUrl, {
+    //     method: 'GET',
+    //     mode: 'no-cors',
+    //     cache: 'no-cache',
+    //     credentials: 'same-origin',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     redirect: 'follow', 
+    //     referrerPolicy: 'no-referrer',
+    // });
+    
+    // return response;
 }
 
-// postFormData().then((data) => {
-//     console.log(data); // JSON data parsed by `data.json()` call
-// });
+
