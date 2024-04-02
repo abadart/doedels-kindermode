dd112e08fded052c84bb0a486a14baf467283a74
Almer Badart <almer.badart@ordina.nl>
2024-04-02 21:20:45 +0200

    Added balloon and form submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,17 @@ const mobileNavToggle = document.getElementById('toggleMainNav');
 const openNavigation = () => {
     mobileNavToggle.setAttribute('aria-expanded', 'true');
     document.body.classList.add('nav-open');
-}
+};
 
 const closeNavigation = () => {
     mobileNavToggle.setAttribute('aria-expanded', 'false');
     document.body.classList.remove('nav-open');
-}
+};
 
 const toggleNavigation = () => {
     const open = mobileNavToggle.getAttribute('aria-expanded');
     open === 'false' ? openNavigation() : closeNavigation();
-}
+};
 
 mobileNavToggle.addEventListener('click', toggleNavigation);
 
@@ -28,36 +28,38 @@ window.addEventListener('keyup', (e) => {
 
 // Mailchimp post and validation
 const newsLetterForm = document.forms.nlForm;
-const mc_c = "jQuery19003303681766368718_";
+const formResponseElm = document.querySelector('.newsletter-form-cta');
+const mc_c = 'jQuery19003303681766368718_';
 
-newsLetterForm.addEventListener("submit", (event) => {
+newsLetterForm.addEventListener('submit', (event) => {
     event.preventDefault();
     sendFormData(event.target);
 });
 
-
 const sendFormData = async (form) => {
-    
     const formData = new FormData(form);
     const params = new URLSearchParams(formData);
-    params.append("c",mc_c+Date.now());
-    params.append("_",Date.now());
+    params.append('c', mc_c + Date.now());
+    params.append('_', Date.now());
 
-    const requestUrl = form.action+"&"+params.toString();
+    const requestUrl = form.action + '&' + params.toString();
 
-    // const response = await fetch(requestUrl, {
-    //     method: 'GET',
-    //     mode: 'no-cors',
-    //     cache: 'no-cache',
-    //     credentials: 'same-origin',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     redirect: 'follow', 
-    //     referrerPolicy: 'no-referrer',
-    // });
-    
-    // return response;
-}
+    try {
+        const response = await fetch(requestUrl, {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
+        if (response){
+            formResponseElm.innerHTML = 'Bedankt voor je inschrijving!';
+            newsLetterForm.style.display = 'none';
+        }
 
+    } catch (error) {
+        console.error('Fout:', error);
+        formResponseElm.innerHTML = 'Er is een fout opgetreden. Probeer het later opnieuw.';
+    }
+};
